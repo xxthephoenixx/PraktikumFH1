8b28c4e8eea43cf3bddf0a9170fab5a330d122bd
Jonathan Robers <jo.robers@web.de>
2021-10-14 18:10:55 +0200

    Finished

--- src/PraktikumFH1.c
+++ src/PraktikumFH1.c
@@ -13,13 +13,13 @@ void addiererInt(){
 	int a = 5, b = 4;
 	int c = a+b;
 	printf("Int Rechner \n");
-	printf("Ergebnis mit Int: %i", c, "%s \n ");
+	printf("Ergebnis mit Int: %i \n", c);
 
 	a = 3.141;
 	b = 2.718;
 	c = a+b;
 
-	printf("Ergebnis mit Float: %f", c, "%s \n ");
+	printf("Ergebnis mit Float: %f \n", c);
 }
 
 void addiereFloat(){
@@ -27,11 +27,11 @@ void addiereFloat(){
 	float c = a+b;
 
 	printf("\nFloat Rechner\n");
-	printf("Ergebnis mit Int: %i", c, "%s \n ");
+	printf("Ergebnis mit Int: %i \n", c);
 
 	a = 3.141;
 	b = 2.718;
 	c = a+b;
 
-	printf("Ergebnis mit Float: %f", c, "%s \n");
+	printf("Ergebnis mit Float: %f \n", c);
 }
